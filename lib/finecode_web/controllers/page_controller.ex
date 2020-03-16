34f6ba420fc5408e177b1b68095e2ba18154379f
yosu <woodstock830@gmail.com>
2020-03-16 10:42:16 +0900

    [remove] phoenix default pages

--- lib/finecode_web/controllers/page_controller.ex
+++ lib/finecode_web/controllers/page_controller.ex
@@ -1,10 +1,6 @@
 defmodule FinecodeWeb.PageController do
   use FinecodeWeb, :controller
 
-  def default(conn, _params) do
-    render(conn, "default.html")
-  end
-
   def index(conn, _params) do
     render(conn, "index.html")
   end
