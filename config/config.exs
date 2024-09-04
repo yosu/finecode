f0f590eccd4fe8da3c2113500f730c839a027a3c
yosu <woodstock830@gmail.com>
2024-09-04 15:33:06 +0900

    Fix logo and favicon not found problem

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,7 @@ config :logger, :console,
 config :esbuild,
   version: "0.12.18",
   default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
