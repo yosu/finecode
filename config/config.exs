274e15196860f7b65784b7b4bb50318864d866dc
yosu <woodstock830@gmail.com>
2024-09-05 14:12:10 +0900

    Migrate config from Mix.Config to Config

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 # Configures the endpoint
 config :finecode, FinecodeWeb.Endpoint,
@@ -41,4 +41,4 @@ config :finecode, :tag_uri, "tag:fine-code.com,2020"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
